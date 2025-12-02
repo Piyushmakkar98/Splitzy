e916dfde83da089e39b933878595838bb5b15b8f
Piyushmakkar98 <152017262+Piyushmakkar98@users.noreply.github.com>
2025-12-02 11:15:38 +0530

    added error handling

--- backend/middleware/verifyLogin.js
+++ backend/middleware/verifyLogin.js
@@ -3,8 +3,9 @@ const jwt = require("jsonwebtoken");
 async function verifyUser(req, res, next) {
   try {
     const authorization = req.headers.authorization;
+    console.log(authorization);
     const token = authorization?.split(" ")[1] || req.cookies.token;
-
+    console.log(token);
     if (!token) throw new Error("No token");
 
     const payload = jwt.verify(token, process.env.JWT_SECRET);
@@ -12,6 +13,7 @@ async function verifyUser(req, res, next) {
     next();
   } catch (error) {
     // Return 401, not redirect
+    console.log(error);
     return res.status(401).json({ message: "Not authenticated" });
   }
 }
